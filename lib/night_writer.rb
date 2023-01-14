9a3f4e3a0f721124ab7477bd7b13ad76a21d80cb
Weston Sandfort <sandfortw@gmail.com>
2023-01-13 17:14:51 -0700

    add night_writer and fixture data

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,5 +2,6 @@ base_file_name = ARGV[0]
 new_file_name = ARGV[1]
 
 file0 = File.open("./data/#{base_file_name}", "r")
-require 'pry'; binding.pry
-p "Created '#{new_file}' containing 11 characters"
+string = file0.read
+
+p "Created '#{new_file_name}' containing #{string.length} characters"
