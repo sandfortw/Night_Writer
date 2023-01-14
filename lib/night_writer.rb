e42a016eec509fd0b2aad8f4d1c7a96f433c9e4a
Weston Sandfort <sandfortw@gmail.com>
2023-01-13 17:39:10 -0700

    Add ability to write a copy of file based on command line inputs

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,5 +3,6 @@ new_file_name = ARGV[1]
 
 file0 = File.open("./data/#{base_file_name}", "r")
 string = file0.read
-File.open("./data/#{new_file_name}", 'w')
+file1 = File.open("./data/#{new_file_name}", 'w')
+file1.write(string)
 p "Created '#{new_file_name}' containing #{string.length} characters"
