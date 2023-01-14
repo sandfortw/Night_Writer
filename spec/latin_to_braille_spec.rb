98f53d0838fb2e826d2a16bb21dec95b64570a2d
Weston Sandfort <sandfortw@gmail.com>
2023-01-14 09:19:20 -0700

    Add a placeholder test for forty_char_break method.

--- spec/latin_to_braille_spec.rb
+++ spec/latin_to_braille_spec.rb
@@ -11,6 +11,11 @@ describe LatinToBraille do
       string = 'hello world'
       expect(LatinToBraille.translate(string)).to eq("O.O.O.O.O....OO.O.O.OO\nOO.OO.O..O..OO.OOOO..O\n....O.O.O....OO.O.O...")
     end
+
+    it 'can break at 40 chars' do
+      string = "the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog"
+      expect(LatinToBraille.forty_char_break(string)).to be_a(String)
+    end
   end
  
 end
