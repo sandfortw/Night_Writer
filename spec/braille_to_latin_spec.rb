e76cfcbc60094d50c8ae342cc3d952fb09cd5d3a
Weston Sandfort <sandfortw@gmail.com>
2023-01-15 11:39:36 -0700

    add tests for unfiltering braille uppercase and numbers

--- spec/braille_to_latin_spec.rb
+++ spec/braille_to_latin_spec.rb
@@ -11,7 +11,19 @@ describe BrailleToLatin do
       string = ["O.O.O.O.O....OO.O.O.OO\n","OO.OO.O..O..OO.OOOO..O\n","....O.O.O....OO.O.O..."]
       expect(BrailleToLatin.translate(string)).to eq('hello world')
     end
+  end
 
+  describe '.unfilter(string)' do
+
+    it 'unfilters upcase' do
+      string = "^h"
+      expect(BrailleToLatin.unfilter(string)).to eq("H")
+    end
+
+    it 'unfilters numbers' do
+      string = '§a'
+      expect(BrailleToLatin.unfilter(string)).to eq("1")
+    end
   end
  
 end
